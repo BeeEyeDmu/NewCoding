26283f2223f6b4272838af9ce8497ebad4b5a51f
BeeEye Dmu <bikang@konyang.ac.kr>
2023-05-12 17:01:15 +0900

    Add files via upload

---
+++ TwoForms/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoForms
+{
+  public partial class Form1 : Form
+  {
+    Form2 f;
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      f = new Form2("제목", this);  // form2에 form1 전달
+      f.Show();     
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      label1.Text = f.textBox1.Text; // Modifier 속성을 public으로 변경
+      MessageBox.Show(f.textBox1.Text); 
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      MessageBox.Show(Common.shared);
+    }
+  }
+
+  public static class Common
+  {
+    public static string shared = "";
+  }
+}
---
+++ TwoForms/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoForms
+{
+  public partial class Form2 : Form
+  {
+    Form1 f = null;
+
+    public Form2(string title, Form1 form)
+    {
+      InitializeComponent();
+      f = form;
+      this.Text = title;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      f.Text = "Form2에서 변경";
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      // Form1의 label1을 modifier에서 public으로 변경
+      f.label1.Text = this.textBox1.Text;
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      Common.shared = this.textBox1.Text;
+    }
+  }
+}
